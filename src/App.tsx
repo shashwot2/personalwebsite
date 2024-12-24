d6a3d002fc2f775f3e6add8127d8d1fdff0a6b56
Shashwot KC <shashwot_07@hotmail.com>
2024-12-24 16:52:18 -0500

    Add basic text like in example

--- src/App.tsx
+++ src/App.tsx
@@ -2,49 +2,38 @@ import { useState, useRef } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
 import { Points, PointMaterial } from '@react-three/drei'
 import * as random from 'maath/random/dist/maath-random.esm'
-import './App.css'
 
-function App() {
+export default function App() {
   const [section, setSection] = useState(0)
   const sections = ['Home', 'Education', 'Projects','Contact', 'Resume']
  
   const handleNextSection = () => {
     setSection((prev) => (prev + 1) % sections.length)
   }
-
   return (
-    <div style={{ position: 'absolute', top: 0, left: 0, width: '100vw', height: '100vh' }}>
       <Canvas camera={{ position: [0, 0, 1] }}>
         <Stars />
       </Canvas>
-    </div>
   )
 }
 
-function Stars({ section }) {
+function Stars(props) {
   const ref = useRef()
   const [sphere] = useState(() => random.inSphere(new Float32Array(5000), { radius: 1.5 }))
-  const cameraRef = useRef()
 
   useFrame((state, delta) => {
     ref.current.rotation.x -= delta / 10
     ref.current.rotation.y -= delta / 15
 
-    if (cameraRef.current) {
-      const targetZ = 3 - section * 1.5
-      state.camera.position.z = THREE.MathUtils.lerp(state.camera.position.z, targetZ, 0.05)
-      state.camera.updateProjectionMatrix()
-    }
   })
 
   return (
     <group rotation={[0, 0, Math.PI / 4]}>
-      <Points ref={ref} positions={sphere} stride={3} frustumCulled={false}>
+      <Points ref={ref} positions={sphere} stride={3} frustumCulled={false} {...props}>
         <PointMaterial transparent color="#ffa0e0" size={0.005} sizeAttenuation={true} depthWrite={false} />
       </Points>
     </group>
   )
 }
 
-export default App
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom'
 import './index.css'
-import App from './App.tsx'
+import App from './App'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+function Overlay() {
+  return (
+    <div style={{ position: 'absolute', top: 0, left: 0, pointerEvents: 'none', width: '100%', height: '100%' }}>
+      <div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate3d(-50%,-50%,0)' }}>
+        <h1 style={{ margin: 0, padding: 0, fontSize: '10em', fontWeight: 500, letterSpacing: '-0.05em' }}>hello!</h1>
+        <h1 style={{ margin: 0, padding: 0, fontSize: '7em', fontWeight: 500, letterSpacing: '-0.05em' }}>Welcome to My Website.</h1>
+      </div>
+      <div style={{ position: 'absolute', top: 40, left: 40, fontSize: '13px' }}>pretty bad —</div>
+      <div style={{ position: 'absolute', bottom: 40, right: 40, fontSize: '13px' }}>25/02/2022</div>
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <>
+    <App /> <Overlay />
+  </>,
+  document.getElementById('root')
 )
+
